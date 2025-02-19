cbba5f59051549b662876b10a9ca95d4a21ec338
Gabriel Gedolin <155395516+GahCyber@users.noreply.github.com>
2025-02-19 12:59:11 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Head from 'next/head';
 import React from "react";
 
-const Portfolio: React.FC = () => {
+export default function Portfolio(): any {
   return (
     <>
       <Head>
@@ -16,18 +16,16 @@ const Portfolio: React.FC = () => {
       <header className="bg-primary text-white text-center p-5">
         <h1>Gabriel Gedolin</h1>
         <p>Desenvolvedor Full-Stack</p>
-        <div>
-          <img alt="HTML" title="HTML" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/html5.svg" />
-          <img alt="CSS" title="CSS" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/css3.svg" />
-          <img alt="JavaScript" title="JavaScript" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/javascript.svg" />
-          <img alt="TypeScript" title="TypeScript" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/typescript.svg" />
-          <img alt="React" title="React" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/react.svg" />
-          <img alt="Next.js" title="Next.js" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/nextdotjs.svg" />
-          <img alt="Bootstrap" title="Bootstrap" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/bootstrap.svg" />
-          <img alt="SASS" title="SASS" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/sass.svg" />
-          <img alt="Git" title="Git" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/git.svg" />
-          <img alt="Python" title="Python" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/python.svg" />
-        </div>
+        <img alt="HTML" title="HTML" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/html5.svg" />
+        <img alt="CSS" title="CSS" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/css3.svg" />
+        <img alt="JavaScript" title="JavaScript" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/javascript.svg" />
+        <img alt="TypeScript" title="TypeScript" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/typescript.svg" />
+        <img alt="React" title="React" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/react.svg" />
+        <img alt="Next.js" title="Next.js" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/nextdotjs.svg" />
+        <img alt="Bootstrap" title="Bootstrap" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/bootstrap.svg" />
+        <img alt="SASS" title="SASS" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/sass.svg" />
+        <img alt="Git" title="Git" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/git.svg" />
+        <img alt="Python" title="Python" width="50px" style={{ paddingRight: '10px' }} src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/python.svg" />
 
         <div>
           <a href="https://github.com" target="_blank" rel="noopener noreferrer">
@@ -45,23 +43,114 @@ const Portfolio: React.FC = () => {
       <section className="container mt-5">
         <h2 className="text-center">Projetos</h2>
         <div className="row mt-4">
-          {/* Loop through your project items if you need to add more */}
-          {[
-            { title: 'Tarefas', link: 'https://tarefas-psi.vercel.app/' },
-            { title: 'Calculadora', link: 'https://calculadora-olive-sigma.vercel.app/' },
-            { title: 'EcoCity', link: 'https://eco-city-eta.vercel.app/' },
-            // Add more project details here
-          ].map((project, index) => (
-            <div className="col-md-4" key={index}>
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">{project.title}</h5>
-                  <p className="card-text">Description of the project</p>
-                  <a href={project.link} className="btn btn-primary">Ver mais</a>
-                </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Tarefas</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="https://tarefas-psi.vercel.app/" className="btn btn-primary">Ver mais</a>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Calculadora</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="https://calculadora-olive-sigma.vercel.app/" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">EcoCity</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="https://eco-city-eta.vercel.app/" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 4</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 5</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 6</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 7</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 8</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 9</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 10</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 11</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Projeto 12</h5>
+                <p className="card-text">ㅤ</p>
+                <a href="#" className="btn btn-primary">Ver mais</a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
@@ -71,5 +160,3 @@ const Portfolio: React.FC = () => {
     </>
   );
 }
-
-export default Portfolio;
