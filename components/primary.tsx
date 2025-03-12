bce1d354b5fce6ea3483f67234c69472a7f02893
Gabriel Gedolin <155395516+GabrielGedolin@users.noreply.github.com>
2025-03-12 12:40:46 -0300

    Create primary.tsx

---
+++ components/primary.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Portfolio(): any {
+  return (
+    <>
+      <div style={{height:"100vh"}} className="w-100 bg-secondary">oi</div>  
+    </>
+  );
+}
