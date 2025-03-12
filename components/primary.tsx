f013dfa2708c7c13676349ce505bdb3e36d46718
Gabriel Gedolin <155395516+GabrielGedolin@users.noreply.github.com>
2025-03-12 13:04:04 -0300

    coloquei a API de dados do github

--- components/primary.tsx
+++ components/primary.tsx
@@ -4,7 +4,25 @@ import React from 'react';
 export default function Portfolio(): any {
   return (
     <>
-      <div style={{height:"100vh"}} className="w-100 bg-secondary">oi</div>  
+      <div style={{height:"100vh"}} className="w-100">
+        <div align="center" style="background-color: #efeaa7; padding: 10px;">
+          <img 
+            align="center" 
+            alt="GitHub Stats" 
+            height="200" 
+            src="https://github-readme-stats.vercel.app/api?username=GahCyber&show_icons=true&theme=highcontrast&include_all_commits=true&locale=pt-br" 
+          />
+        </div>
+        <br/>
+        <div align="center" style="background-color: #efeaa7; padding: 10px;">
+          <img 
+            align="center" 
+            alt="GitHub Stats" 
+            height="200" 
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=GahCyber&theme=highcontrast&layout=compact&custom_title=Tecnologias&langs_count=9" 
+          />
+        </div>
+      </div>  
     </>
   );
 }
