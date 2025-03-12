d343697efa45291509b74bc3691654db945844b4
Gabriel Gedolin <155395516+GabrielGedolin@users.noreply.github.com>
2025-03-12 20:50:38 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import React from 'react';
-import Primary from "../components/primary.tsx"
+import Primary from "../components/primary"
 export default function Portfolio(): any {
   return (
     <>
